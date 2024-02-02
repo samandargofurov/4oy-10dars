49c42f602d14c05af97ff984e09327bf8b657087
samandargofurov <samandargofurov236@gmail.com>
2024-02-02 12:09:47 +0500

    10 dars

---
+++ index.js
@@ -0,0 +1,44 @@
+// 1. Berilgan sonlardan tashkil topgan massivdan 10 dan kattalaridan iborat yangi massiv hosil qiluvchi funksiya. \/\/
+
+// let arr = [10, 11, 21, 6, 7, 3, 1, 4, 76, 98, 2, 4, ];
+
+// let newArr = arr.filter((v) => {
+//     return v > 10
+// });
+
+// console.log(newArr);
+
+// 2. Sonlardan tashkil topgan massiv elementlari kublaridan iborat yangi massiv hosil qiladigan funksiya yozing. \/\/
+
+// let massive = [2, 3, 1, 5, 6];
+
+// let elmKubi = massive.map((value) => {
+//     return value ** 3
+// });
+
+// console.log(elmKubi);
+
+// 3. Sonlardan tashkil topgan massiv elementlari orasidan 3 ga hamda 5 ga karrali son bor yoki yoqligini tekshiruvchi funksiya yozing.
+
+
+
+// 4. Sonlardan tashkil topgan massiv elementlari orasidan 3-eng kattasini topuvchi funksiya yozing.
+
+// let arr = [8, 7, 6, 3, 11, 10, 13];
+
+// let arr1 = arr.sort((a, b) => {
+//     return b - a
+// });
+
+// console.log(arr1);
+// console.log(arr1[2]);
+
+// 5. Sonlardan tashkil topgan massiv teskari tartibda yozib uni yozuv korinishiga o'tkazib beruvchi funksiya yozing.
+
+// let massive = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+
+// let arr1 = massive.reverse().map((v) => {
+//     return v.toString();
+// });
+
+// console.log(arr1);
